291778a6f79a965534616a16550e0ef8ce5c69eb
fandix huang <fandix@weiruideMacBook-Pro.local>
2021-06-06 19:38:56 +0800

    working with EJS

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,22 @@
-const path = require('path');
 const express = require('express');
 
 const router = express.Router();
+const products = [];
+
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
-});
-
-router.post('/add-product', (req, res, next) => {
-    console.log(req.body);
+    res.render('add-product', {
+      pageTitle: 'Add Product',
+      path: '/admin/add-product',
+      formsCSS: true,
+      productCSS: true,
+      activeAddProduct: true
+    });
+  });
+  
+  router.post('/add-product', (req, res, next) => {
+    products.push({ title: req.body.title });
     res.redirect('/');
-});
+  });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
