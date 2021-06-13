85ce2437afbe9cbfd2af0a4138d8ac6840b47166
fandix huang <fandix@weiruideMacBook-Pro.local>
2021-06-13 12:56:19 +0800

    Use Sequelize to create SQL

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'How840925'
+const sequelize = new Sequelize('node-complete', 'root', 'How840925', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
